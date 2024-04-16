39d33d6a0553de3100612c8bd8b658ff7bdde592
Hee Suk Shin <heesuk@gmail.com>
2024-04-16 13:26:48 +0900

    Heapless example.

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use aes_gcm::{
+    aead::{heapless::Vec, AeadCore, AeadInPlace, KeyInit, OsRng},
+    Aes256Gcm,
+};
+use anyhow::Error;
+
+fn main() -> Result<(), Error> {
+    let key = Aes256Gcm::generate_key(&mut OsRng);
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
+
+    let mut buffer: Vec<u8, 128> = Vec::new(); // Note: buffer needs 16-bytes overhead for auth tag
+    buffer
+        .extend_from_slice("plaintext message".as_bytes())
+        .expect("buffer is full");
+
+    // Encrypt `buffer` in-place, replacing the plaintext contents with ciphertext
+    cipher.encrypt_in_place(&nonce, b"", &mut buffer)?;
+
+    // `buffer` now contains the message ciphertext
+    assert_ne!(&buffer, b"plaintext message");
+
+    // Decrypt `buffer` in-place, replacing its ciphertext context with the original plaintext
+    cipher.decrypt_in_place(&nonce, b"", &mut buffer)?;
+    assert_eq!(&buffer, b"plaintext message");
+    Ok(())
+}
